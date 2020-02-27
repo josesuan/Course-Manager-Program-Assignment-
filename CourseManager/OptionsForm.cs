46bd20a546a6463f9e1676e6946ba5ebfdc3acd3
josesuan <60651809+josesuan@users.noreply.github.com>
2020-02-27 14:20:45 +0000

    Add files via upload
    Added main menu form

---
+++ CourseManager/OptionsForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseManager
+{
+    public partial class OptionsForm : Form
+    {
+        Module currentModule;
+
+		Dashboard Dashboard = new Dashboard(); //Initialised object
+
+		public OptionsForm(Module mod)
+        {
+            InitializeComponent();
+            currentModule = mod;
+
+        }
+
+        private void Bt_edit_Click(object sender, EventArgs e)
+        {
+
+        }
+		
+
+        private void Bt_sendPending_Click(object sender, EventArgs e)
+        {
+			//Dashboard.removeOldLocation(currentModule); //Remove module in Ongoing
+			
+			Dashboard.AddToPending(currentModule); // Adds selected module in dgv pending
+			this.Close();
+		}
+
+		//Continue here!
+		private void Bt_remove_Click(object sender, EventArgs e)
+		{
+			//Remove in the dgv 
+			Dashboard dashboard = new Dashboard();
+
+
+			//Remove in object class
+			Dashboard.removeOldLocation(currentModule);
+			Dashboard.moduleList.Remove(currentModule);
+			this.Hide();
+		}
+
+		private void Bt_sendFinish_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
