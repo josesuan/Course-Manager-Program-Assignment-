5cfc0ca268db8da90ffccea7092573226f14859f
LowUp <60651716+LowUp@users.noreply.github.com>
2020-02-27 19:31:28 +0000

    Edit Form

---
+++ Nachos_SoftwareEngineerAssignment-Emereole Chinduji- Edit form/CourseManager/Module.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManager
+{
+    public class Module
+    {
+
+        public string year { get; set; }
+        public string moduleName { get; set; }
+        public int assignmentNum { get; set; }
+        public string assigmentType { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime dueDate { get; set; }
+        public string location { get; set; }
+        public int mark { get; set; }
+
+
+		//Constructors
+		public Module()
+		{
+
+		}
+
+			public Module(string Year, string Modulename, int AssignmentNum, string AssignmentType, DateTime StartDate, DateTime DueDate, string Location)
+		{
+			year = Year;
+			moduleName = Modulename;
+			assignmentNum = AssignmentNum;
+			assigmentType = AssignmentType;
+			startDate = StartDate;
+			dueDate = DueDate;
+			location = Location;
+		}
+        public Module(string Year, string Modulename, int AssignmentNum, string AssignmentType, DateTime StartDate, DateTime DueDate, string Location, int grade)
+        {
+            year = Year;
+            moduleName = Modulename;
+            assignmentNum = AssignmentNum;
+            assigmentType = AssignmentType;
+            startDate = StartDate;
+            dueDate = DueDate;
+            location = Location;
+            mark = grade;
+        }
+
+
+
+
+        public int TimeRemaining()
+        {
+            DateTime currentDate;
+            currentDate = DateTime.Today;
+            int timeRemaining;
+
+            timeRemaining = dueDate.DayOfYear - currentDate.DayOfYear;
+
+            return timeRemaining;
+        }
+
+	}
+}
+
---
+++ Nachos_SoftwareEngineerAssignment-Emereole Chinduji- Edit form/CourseManager/OptionsForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseManager
+{
+    public partial class OptionsForm : Form
+    {
+        Module currentModule;
+
+		Dashboard Dashboard = new Dashboard(); //Initialised object
+
+		public OptionsForm(Module mod)
+        {
+            InitializeComponent();
+            currentModule = mod;
+
+        }
+
+        private void Bt_edit_Click(object sender, EventArgs e)
+        {
+            EditForm formEdit = new EditForm(currentModule);
+            formEdit.Show();           
+            OptionsForm optionForm = new OptionsForm(currentModule);
+            this.Close();
+            
+
+        }
+		
+
+        private void Bt_sendPending_Click(object sender, EventArgs e)
+        {
+			//Dashboard.removeOldLocation(currentModule); //Remove module in Ongoing
+			
+			//Dashboard.AddToPending(); // Adds selected module in dgv pending
+			this.Close();
+		}
+
+		//Continue here!
+		private void Bt_remove_Click(object sender, EventArgs e)
+		{
+			//Remove in the dgv 
+			Dashboard dashboard = new Dashboard();
+
+
+			//Remove in object class
+			Dashboard.removeOldLocation(currentModule);
+			Dashboard.moduleList.Remove(currentModule);
+			this.Hide();
+		}
+
+		private void Bt_sendFinish_Click(object sender, EventArgs e)
+		{
+            
+
+        }
+	}
+}
